cd3126cb59745e81257ee501e9ccc473f4044085
MichaelJava69 <Michael.professional@inbox.com>
2018-02-17 23:21:15 +0000

    Upload

---
+++ module-a/src/test/java/org/example/ATest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.Test;
+
+public class ATest {
+
+    @Test
+    public void a() throws Exception {
+        new A().foo();
+    }
+    
+     
+    public void testC() {
+        new A().bar();
+    }
+
+}
